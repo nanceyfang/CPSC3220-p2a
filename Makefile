fc8f76ec747bb2ef394f63b778d11c028ffabcbb
Nancey Fang <nfang@ada4.computing.clemson.edu>
2019-02-19 21:22:53 -0500

    added cd to a certain directory; added built in executables in /bin/ but it's hard coded; still need to account for arguments

--- Makefile
+++ Makefile
@@ -1,6 +1,7 @@
 all: whoosh
 
 whoosh: whoosh.c
+	PATH=$PATH\:/bin ; export PATH
 	gcc -g -Wall -Wextra whoosh.c -o whoosh
 
 clean:
--- whoosh.c
+++ whoosh.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <dirent.h>
+#include <stdbool.h>
 
 #define MAX_LEN 128
 
@@ -10,10 +12,12 @@ int main(){
   
   char error_message[30] = "An error has occurred\n";
 
+
   //write(STDERR_FILENO, error_message, strlen(error_message));
 
   //char *input = malloc(sizeof(char)*MAX_LEN);
   char input[MAX_LEN];
+	bool built_in = false;
 
   const char space[4] = " \t\n";
 
@@ -28,15 +32,16 @@ int main(){
     pid_t pid = 1; // just to initialize for whoosh>
 
     while (token!=NULL){
-   
-      // check if input already implemented
-
+  		 
       if ( !(strcmp(token, "exit")) ){
         //free(input);
         exit(0);
       }
 
+			built_in = false;
+
       if ( !(strcmp(token, "pwd")) ){
+				built_in = true;
         pid = fork();
         //parent process
         if (pid){
@@ -52,17 +57,72 @@ int main(){
 
       }
 
-      if ( !(strcmp(token, "cd")) ){
+			if ( !(strcmp(token, "path")) ){
+				built_in = true;
+				//uh i think i need to fix this somehow
+				printf("%s\n", getenv("PATH") );
+      }
+
+			if ( !(strcmp(token, "cd")) ){
+					built_in = true;
           token = strtok(NULL, space);
           if (token == NULL){
             chdir( getenv("HOME") );
           }
+					else{ // cd to token
+						chdir( token );
+					}
       }
 
+			if (token!=NULL && !built_in){ 
+      	// check if input already implemented
+				// go to /bin or /usr/bin and check
+				DIR *d; // like a file?
+				struct dirent *directory;
+				d = opendir("/bin");
+				if(d){
+					directory=readdir(d);
+					while (directory != NULL){
+						// command in /bin
+						if ( !(strcmp(token,directory->d_name)) ){
+							//printf("match");
+
+							int argc = 0;
+							
+							printf("%s\n", input);
+
+							pid = fork();
+							if (pid){
+								int status;
+			          waitpid(pid, &status, 0);
+							}
+							else{
+								char command[MAX_LEN];
+								strcpy(command, "/bin/");
+								strcat(command, token);
+
+								// go through input for arguments?
+								for (int i = 0; i < strlen(input); i++){
+									printf("%c", input[i]);
+								}			
+
+								char *args[] = {token, NULL, NULL, NULL};
+								execv(command, args);
+							}			
+							break;
+						}		
+						directory=readdir(d);
+					}
+					closedir(d);
+				}
+			}
+
       token = strtok(NULL, space);
+
       if (pid){
         printf("whoosh> ");
       }
+
     }
   }
 
